be555e4819228b3609ffb4355b9e55188287d212
Mutesi <joanahmutesi38@gmail.com>
2025-08-01 09:00:04 +0300

    Python tests

---
+++ hello.sh
@@ -0,0 +1,12 @@
+# #!/bin/bash
+
+# echo "Enter numbers (separated by spaces):"
+# read -a numbers
+
+# for num in "${numbers[@]}"; do
+#     if (( num % 2 == 0 )); then
+#         echo "$num is Even"
+#     else
+#         echo "$num is Odd"
+#     fi
+# done
